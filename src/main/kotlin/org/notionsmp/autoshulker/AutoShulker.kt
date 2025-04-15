df91011b37b2f4dc97a5a0fc89e1f2a3204abb8c
Richard Mikloš <akisytbsteamy@gmail.com>
2025-04-15 21:00:32 +0200

    refactor: Remove unused import

--- src/main/kotlin/org/notionsmp/autoshulker/AutoShulker.kt
+++ src/main/kotlin/org/notionsmp/autoshulker/AutoShulker.kt
@@ -1,6 +1,5 @@
 package org.notionsmp.autoshulker
 
-import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
 import org.bukkit.Material
